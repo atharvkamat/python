6db86e00a17baa8a229002dac1381dbea271f051
atharvkamat <39836357+atharvkamat@users.noreply.github.com>
2020-04-27 11:37:54 +0530

    version 1`

---
+++ bubble_sort.py
@@ -0,0 +1,34 @@
+num_test_cases = int(input())
+def lenght(list_or_string):
+    try:
+        c = True
+        y = 0
+        while c== True:
+            y=y+1
+            u = list_or_string[y]
+    except IndexError:
+        c= False
+    return int(y)
+def co(s):
+    str1 = [int(x) for x in s.split(' ') ]
+    return str1
+def d(sty):
+    li1 = ' '
+    li = ' '.join([str(e) for e in sty])
+    return li
+def num_finder(mylist):
+    indexlen = lenght(mylist)
+    yq=indexlen+1
+    n=indexlen-1 
+    for i in range(yq):
+        u = -yq+6
+        for j in range(u):
+            if mylist[j]>mylist[j+1]:
+                mylist[j],mylist[j+1] = mylist[j+1],mylist[j]    
+    return mylist
+
+for i in range(0,num_test_cases) :
+    wnum_elements = input()
+    in_string = input()
+    my_list = co(in_string)
+    print(d(num_finder(my_list)))
